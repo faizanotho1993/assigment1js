3cd3bd35f84065d36df5b5861a3c7555d01ff3b2
faizanotho1993 <faizanullahotho@gmail.com>
2023-10-15 02:30:45 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,114 @@
+//....................... Chapter1 ALERT............
+// Q1
+
+// alert("hello world")
+
+//Q2 
+// alert("Error! please enter a valid password")
+
+//Q3
+// alert("Wellcome to JS land.. \n happy coding!")
+
+// Q4
+// alert("well come to js land..")
+// alert("happy codding")
+
+
+// Q5
+// console.log("hello i can run js through web browser console") 
+// document.write("hello i can run js through web browser console") 
+
+
+//................................CHAPTER 2 VARIABLE.........
+// Q1
+// var userName = "Faizan"
+// alert(userName)
+
+// Q2
+// var myName = "faizan"
+// var lastName = "otho"
+// var fullName = myName + " " + lastName 
+// alert(fullName)
+
+
+// Q3
+// var message = ;
+// var message = "helloworld"
+// alert(message)
+
+// Q4
+// var userName = "jhon doe"
+// var age = "15 year old"
+// var certificate = "certificate mobile application devlopment"
+
+
+// alert(userName)
+// alert(age)
+// alert(certificate)
+
+// Q5
+// var name = " PIZZA \n PIZZ \n PIZ \n PI \n P" 
+// alert(name)
+
+// Q6
+// var email = "faizanullahotho@gmail.com"
+// var MyEmailis = email
+// alert=("MyEmailis + email")
+
+
+// var words = "Yah!  I can  Write HTML content through Java script"
+// document.write("Yah!  I can  Write HTML content through Java script")
+
+
+// Q9
+// var pattern = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” "
+// alert(pattern)
+
+
+// ..................CHAPTER # 3 VARIABLE FOR NUMBERS..............
+
+
+// Q1
+// var age ="i m 15 year old"
+// alert(age)
+
+// Q2
+// var numberOfVisit=14
+// alert("you have visited this site" + numberOfVisit)
+
+
+// Q3
+// var birthYear = "1993"
+// var message ="my birth year is"
+// var msg = "data type of my ddecalared variable is number"
+// document.write(message + birthYear + msg)
+
+// // Q4
+// var visitorName = "John Doe"
+// var productTitle = "T Shirt(s)"
+// var Quantity = "Ordered 5"
+// var companyName = "XYZ clothing company"
+// document.write(visitorName + Quantity + productTitle + companyName)
+
+
+// ..............CHAPTER 4 VARIABLE NAME LEGAL AND ILLEGAL................
+
+// var a,b,c ;
+
+// legal variables $ 1st _ 
+
+//ILEGAL variable..
+
+// 123 @#@000
+
+// var message = "Variable names can only  Contain , numbers $ and _. For example : $my _1st Variable"
+// document.write(message)
+
+// var second = "Variable must beign with a letter ,$ or _ For example : $name _name or name "
+// document.write(second)
+
+// var third ="variable name or case sensitive "
+// document.write(third)
+
+// var four ="variable name can should not be JS  key words"
+// document.write(four)
